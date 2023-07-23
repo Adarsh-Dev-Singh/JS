9ad58feedb9d00acff15da66e7975bc34e21882c
J-ON-AS <118046576+J-ON-AS@users.noreply.github.com>
2023-07-23 16:13:10 +0530

    Game updated

--- Booleans And If else/script.js
+++ Booleans And If else/script.js
@@ -1,5 +1,5 @@
-let choice = '';
 function comChoice(){
+  let choice = '';
   let Random = Math.random();
 if(Random >= 0 && Random < 1/3 ){
  choice  = 'Rock';
@@ -8,8 +8,40 @@ if(Random >= 0 && Random < 1/3 ){
 }else if(Random >= 2/3 && Random < 1 ){
  choice  = 'Scissor';
 }
+return choice ;  
 } 
 comChoice();
+function playGame(playerMove){
+      if(playerMove == 'Rock'){
+    if(choice === 'Rock'){
+      result = 'Tie' ;
+    }else if(choice === 'Paper' ){
+      result = 'You Lose!' ;
+    }else if(choice === 'Scissor' ){
+      result = 'You Win!'  ;
+    }
+
+  }else if(playerMove == 'Paper'){
+      if(choice === 'Paper'){
+        result = 'Tie' ;
+        }else if(choice === 'Rock' ){
+          result = 'You Lose!' ;
+        }else if(choice === 'Scissor' ){
+          result = 'You Win!'  ;        
+        }
+
+  }else if(playerMove == 'Scissor'){
+      if(choice === 'Scissor'){
+      result = 'Tie' ;
+    }else if(choice === 'Rock' ){
+      result = 'You Lose!' ;
+    }else if(choice === 'Paper' ){
+      result = 'You Win!'  ;
+    }
+}
+alert(`You Picked ${playerMove} and Computer Picked ${choice} . ${result}` )
+}
+
 // location.reload();
 
 // console.log(choice);
