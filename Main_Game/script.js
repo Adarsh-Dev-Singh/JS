65639a6dd80cd736a896bdb531808a6897e976f0
J-ON-AS <118046576+J-ON-AS@users.noreply.github.com>
2023-07-29 13:13:22 +0530

    JSON and localStorage builtin objects Auto boxing and some shorthand notations

--- Main_Game/script.js
+++ Main_Game/script.js
@@ -1,13 +1,20 @@
-const score = {
-  win : 0,
+let score = JSON.parse(localStorage.getItem('score')) || { // Default operator used
+  tie : 0,
   loss : 0,
-  tie : 0
-};
+  win : 0
+};//JSON AND localStorage used
 
+// if(!score){ // score === null  = !score
+//   score ={
+//     tie : 0,
+//     loss : 0,
+//     win : 0
+//   }
+// }
 
 function comChoice(){
   let choice = '';
-  let Random = Math.random();
+  let Random = Math.random(); //random method from Math built-in object is used
 if(Random >= 0 && Random < 1/3 ){
  choice  = 'Rock';
 }else if(Random >= 1/3 && Random < 2/3 ){
@@ -56,8 +63,11 @@ if(result === 'Tie'){
 }else if(result === 'You Win!'){
   score.win ++;
 }
+
+localStorage.setItem('score',JSON.stringify(score));//JSON AND localStorage used
+
 alert(`You Picked ${playerMove} and Computer Picked ${choice} . ${result}
-Win : ${score.win} Loss : ${score.loss} Tie : ${score.tie}` );
+Win : ${score.win} Loss : ${score.loss} Tie : ${score.tie}` );//Template String used
 }
 
 // location.reload();
--- Objects/script_new.js
+++ Objects/script_new.js
@@ -1,20 +1,73 @@
 
-const beverageDrink = {
-    coffee : "Cappucino : $120 + tax for 100ml",
-    tea : "$1200 for 100ml",
-    lassi : "Meethi Lassi : $12000 + tax for 100ml",
+// const beverageDrink = {
+//     coffee : "Cappucino : $120 + tax for 100ml",
+//     tea : "$1200 for 100ml",
+//     lassi : "Meethi Lassi : $12000 + tax for 100ml",
 
+// };
+
+// console.log(beverageDrink.coffee);
+// console.log(beverageDrink.lassi);
+// console.log(beverageDrink.tea);
+
+// beverageDrink.tea = 'Masala Chai : $1250 + tax for 10ml '
+// console.log(beverageDrink);
+
+// beverageDrink.coldDrink = '$9999999 + tax for 2ml Cold Drink of your choice';
+// console.log(beverageDrink); 
+
+// delete beverageDrink.coffee;
+// console.log(beverageDrink); 
+
+// const logan = {
+//     name : 'Wolverine',
+//     ability:{
+//         superPower:'healing',
+//         strength:'claws'
+//     }
+// };
+
+// console.log(logan['ability']['superPower']); // Bracket Notation
+
+// const a = JSON.stringify(logan);//JSON 
+// console.log(a);
+// // localStorage is not done here 
+// // JSON.parse(a);
+// console.log(JSON.parse(a));
+// //objects are just refrences if we take
+
+const lassi ={
+    flavour : 'meethi',
+    rate : 10,
+    quantity : '100ml'
+}; // The object lassi is just a refrence to a partucular location in computer's memory where all these property value pairs are stored 
+
+const bassi = lassi; // This doesn't make copy of lassi object in bassi but makes a copy of refrence to that location so bassi and lassi points to the same location in memory . Also known as Copy by refrence
+
+const massi = {
+    flavour : 'meethi',
+    rate : 10,
+    quantity : '100ml',
+    // other : function function1(){
+    //     console.log('Hello  Longhand method notation');
+    // },
+    other(){
+        console.log('Hello Shorthand method notation');
+    }
 };
 
-console.log(beverageDrink.coffee);
-console.log(beverageDrink.lassi);
-console.log(beverageDrink.tea);
+console.log(massi === lassi); // This will give false because they are not refrencing to the same location in memory
 
-beverageDrink.tea = 'Masala Chai : $1250 + tax for 10ml '
-console.log(beverageDrink);
+console.log(bassi === lassi);
 
-beverageDrink.coldDrink = '$9999999 + tax for 2ml Cold Drink of your choice';
-console.log(beverageDrink); 
+// const flavour = lassi.flavour;
+const { flavour , rate } = lassi;// Destructuring an object 
+console.log(flavour); // So the Destruction shortcut took the flavour out of the lassi and saved it in the lassi variable
+console.log(rate);
 
-delete beverageDrink.coffee;
-console.log(beverageDrink); 
+const jassi = {
+    // flavour : flavour// This will take whatever is inside flavour variable and will substitute it here
+    flavour // This will do same thing 
+}
+console.log(jassi);
+massi.other();
